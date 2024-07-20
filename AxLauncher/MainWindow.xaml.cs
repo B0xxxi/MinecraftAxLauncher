0f07a4bffe24524518a164141bded7bc3076dbac
B0xi <ivan0polyah@gmail.com>
2024-07-20 22:27:44 +0300

    Убран легаси код. Добавлена загрузка и запуск майнкрафта с форджом.

--- AxLauncher/MainWindow.xaml.cs
+++ AxLauncher/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MaterialDesignColors;
-using MaterialDesignThemes;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
@@ -25,30 +23,70 @@ using CmlLib.Core.Auth;
 using CmlLib.Core.Installer.Forge;
 using CmlLib.Core.Installers;
 using CmlLib.Core.ProcessBuilder;
+using Renci.SshNet.Common;
 
 namespace AxLauncher
 {
     public partial class MainWindow : Window
-
     {
-        private async void Play_Click(object sender, RoutedEventArgs e)
+        public MainWindow()
         {
-            var path = new MinecraftPath(); // use default directory
+            InitializeComponent(); // Инициализация окна
+        }
+
+        void Border_MouseLeftButtonDown(object localSender, MouseButtonEventArgs e)
+        {
+            this.DragMove(); // Перенос окна
+        }
+
+        void CloseButton_Click(object localSender, RoutedEventArgs e)
+        {
+            this.Close(); // Это закроет текущее окно
+        }
+
+        // Логин\Никнейм
+        string login = ""; // Создаём переменную login
+        void Login_TextChanged(object localSender, TextChangedEventArgs e)
+        {
+            login = Login.Text; // Задача переменной login слайдером
+            Console.WriteLine($"Login changed: {login}");
+        }
+
+        // Пароль (Может вообще убрать его и сделать авторизацию прямо на сервере через мод? Надо обсудить)
+        void Pass_TextChanged(object localSender, TextChangedEventArgs e)
+        {
+
+        }
+
+        // Оператива
+        int operdata = 4096; // Создаём переменную с оперативой (Пусть по умолчанию будет 4096)
+        void Slider_ValueChanged(object localSender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            operdata = (int)e.NewValue; // Задаем значение переменной operdata
+            Console.WriteLine($"RAM: {operdata}"); // Выводим в консоль кол-во выбраной оперативки
+        }
+
+        // Кнопка играть (Запуск игры)
+        public async void Play_Click(object localSender, RoutedEventArgs e)
+        {
+            System.Net.ServicePointManager.DefaultConnectionLimit = 256;
+
+            var path = new MinecraftPath(); // Стандартная директория
             var launcher = new MinecraftLauncher(path);
 
-            // show launch progress to console
-            var fileProgress = new SyncProgress<InstallerProgressChangedEventArgs>(e =>
-                Console.WriteLine($"[{e.EventType}][{e.ProgressedTasks}/{e.TotalTasks}] {e.Name}"));
-            var byteProgress = new SyncProgress<ByteProgress>(e =>
-                Console.WriteLine(e.ToRatio() * 100 + "%"));
-            var installerOutput = new SyncProgress<string>(e =>
-                Console.WriteLine(e));
+            // Лог загрузки в консоль
+            var fileProgress = new SyncProgress<InstallerProgressChangedEventArgs>(x =>
+                Console.WriteLine($"[{x.EventType}][{x.ProgressedTasks}/{x.TotalTasks}] {x.Name}"));
+            var byteProgress = new SyncProgress<ByteProgress>(x =>
+                Console.WriteLine(x.ToRatio() * 100 + "%"));
+            var installerOutput = new SyncProgress<string>(x =>
+                Console.WriteLine(x));
 
-            //Initialize variables with the Minecraft version and the Forge version
+            //Задаём версию майна и форджа
             var mcVersion = "1.20.1";
-            var forgeVersion = "47.2.32";
+            var forgeVersion = "47.3.0";
 
-            //Initialize MForge
+            //Инициализируем MForge
             var forge = new ForgeInstaller(launcher);
 
             var version_name = await forge.Install(mcVersion, forgeVersion, new ForgeInstallOptions
@@ -57,21 +95,21 @@ namespace AxLauncher
                 ByteProgress = byteProgress,
                 InstallerOutput = installerOutput,
             });
-            //var version_name = await forge.Install(mcVersion); // install the recommended forge version for mcVersion
-            //OR var version_name = forge.Install(mcVersion, forgeVersion).GetAwaiter().GetResult();
 
-            //Start Minecraft
+            login = Login.Text;
+
+            //Запуск майна
             var launchOption = new MLaunchOption
             {
-                MaximumRamMb = 1024,
-                Session = MSession.CreateOfflineSession("Gamer123"),
+                MaximumRamMb = operdata,
+                Session = MSession.CreateOfflineSession(login),
             };
 
-            var process = await launcher.InstallAndBuildProcessAsync(version_name, launchOption);
+            Process process = await launcher.InstallAndBuildProcessAsync(version_name, launchOption);
 
-            // print game logs
+            // Лооооооги
             var processUtil = new ProcessWrapper(process);
-            processUtil.OutputReceived += (s, e) => Console.WriteLine(e);
+            processUtil.OutputReceived += (s, x) => Console.WriteLine(x);
             processUtil.StartWithEvents();
             await processUtil.WaitForExitTaskAsync();
         }
